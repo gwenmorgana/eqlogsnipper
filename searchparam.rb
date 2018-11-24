a6d669a1f7f873d06601e3c0ca098a4b2358b5d7
gwenmorgana <gwenmorgana@gmail.com>
2018-11-23 16:56:39 -0800

    Searchparameter PH Module created.

---
+++ searchparam.rb
@@ -0,0 +1,10 @@
+# Additional search parameter Module
+
+module Searchparam
+  def Searchparam
+    puts "This is the Searchparam module"
+  end
+
+  # Just a variable to prepare
+  SEARCHLIMITER = "A PH Variable"
+end
